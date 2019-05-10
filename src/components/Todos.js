3c24a6feafd126b53a1138180734a32406a4ba1c
Ronyonka <rononyonka@gmail.com>
2019-05-10 14:23:55 +0300

    added components

---
+++ src/components/Todos.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+function Todos() {
+  return (
+    <div>
+      <h1>Todo</h1>
+    </div>
+  );
+}
+
+export default Todos;
+
+
+// import React, { Component } from 'react';
+//
+//
+// class Todos extends Component {
+//   render(){
+//     return (
+//       <div>
+//         <h1>Todos</h1>
+//       </div>
+//     );
+//   }
+//   }
+//
+// export default Todos;
