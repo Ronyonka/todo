e756af03f9edd42fa8e27d3ef72faed99fa24516
Ronyonka <rononyonka@gmail.com>
2019-05-10 15:35:51 +0300

    added states props and components

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
+class Todos extends Component {
+  render(){
 
-function Todos() {
-  return (
-    <div>
-      <h1>Todo</h1>
-    </div>
-  );
-}
+    return this.props.todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo}/>
+    ));
+  }
+  }
 
 export default Todos;
 
 
-// import React, { Component } from 'react';
+// import React from 'react';
 //
 //
-// class Todos extends Component {
-//   render(){
-//     return (
-//       <div>
-//         <h1>Todos</h1>
-//       </div>
-//     );
-//   }
-//   }
+// function Todos() {
+//   return (
+//     <div>
+//       <h1>Todo</h1>
+//     </div>
+//   );
+// }
 //
 // export default Todos;
