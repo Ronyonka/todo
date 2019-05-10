50f747f98901640c5d4d546a2084285123cca79e
Ronyonka <rononyonka@gmail.com>
2019-05-10 16:38:08 +0300

    added toggle complete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ?
+      'line-through' : 'none'
+    }
+  }
+  // getStyle = () => {
+  //   if(this.props.todo.completed) {
+  //     return {
+  //       textDecoration: 'line-through'
+  //     }
+  //   }else{
+  //     return {
+  //       textDecoration: 'none'
+  //     }
+  //   }
+  // }
+
+  markComplete = (e) => {
+    console.log(this.props)
+  }
+
   render() {
+    const { id, title } = this.props.todo;
     return(
-      <div>
-      <p>{ this.props.todo.title }</p>
+      <div style={this.getStyle()}>
+      <p>
+      <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/>{' '}
+      { title }
+      </p>
       </div>
     )
   }
 }
+
+// PropTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+
+// const itemSyule = {
+//   backgroundColor: '#f4f4f4'
+// }
 
 export default TodoItem;
