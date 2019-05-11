109ec6fbe6629726dc435668f6265fa26e5d2de2
Ronyonka <rononyonka@gmail.com>
2019-05-11 16:01:28 +0300

    Updates

---
+++ precache-manifest.9446bf5e09288fdd2f251a2100e24673.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c82b263c88c17787f8d34fe879709b",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "963fb94706698863ae04",
+    "url": "/todo/static/css/main.b9e13aed.chunk.css"
+  },
+  {
+    "revision": "94c3165186f215582bb2",
+    "url": "/todo/static/js/2.c37d2ede.chunk.js"
+  },
+  {
+    "revision": "963fb94706698863ae04",
+    "url": "/todo/static/js/main.8c36afa1.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
